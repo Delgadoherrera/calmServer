b56293f04689cd51c0d078b14d540d24c9ff620e
delgadoHerrera <delgado.herrera.luciano@gmail.com>
2023-03-19 04:06:21 -0300

    pacientesApi

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(session({ secret: 'Calm Salute' }));
 app.use(express.json({limit: "10mb", extended: true}))
 app.use(express.urlencoded({limit: "10mb", extended: true, parameterLimit: 50000}))
 
-app.use(cors())
+app.use(cors({ origin: '*' }))
 
 app.use('/', userApi);
 app.use('/', curriculumApi);
